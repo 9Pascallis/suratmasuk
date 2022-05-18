13df25749847a33efc697ead553339428f56cf23
indahlutfiyah <indahlutfiyah30@gmail.com>
2022-05-18 10:38:29 +0700

    tambah data

--- tambahsurat.php
+++ tambahsurat.php
@@ -1,6 +1,10 @@
 <?php
+
 require 'database/db.php';
+// include "database/db.php";
+// require_once ('modals/m.suratmasuk.php');
 session_start();
+
 ?>
 
 <!DOCTYPE html>
@@ -44,13 +48,13 @@ $(document).ready(function(){
                     </div>
                 </div>
             </div>
-            <form>
+            <form method="post" enctype="multipart/form-data">
             <div class="row">
                 <div class="col-sm-12">
                     <div class="form-group row">
                         <label for="colFormLabel" class="col-sm-2 col-form-label">No Agenda Sekarang</label>
                         <div class="col-sm-9">
-                        <input type="text" class="form-control" id="colFormLabel" placeholder="">
+                        <input type="text" class="form-control" name="no_agenda" id="no_agenda" placeholder="">
                         </div>
                     </div>
                 </div>
@@ -60,7 +64,7 @@ $(document).ready(function(){
                     <div class="form-group row">
                         <label for="colFormLabel" class="col-sm-2 col-form-label">No Surat</label>
                         <div class="col-sm-9">
-                        <input type="text" class="form-control" id="colFormLabel" placeholder="">
+                        <input type="text" class="form-control" name="no_surat" id="no_surat" placeholder="">
                         </div>
                     </div>
                 </div>
@@ -70,7 +74,7 @@ $(document).ready(function(){
                     <div class="form-group row">
                         <label for="colFormLabel" class="col-sm-4 col-form-label">Tanggal Agenda</label>
                         <div class="col-sm-7">
-                        <input type="date" class="form-control" id="colFormLabel" placeholder="">
+                        <input type="date" class="form-control" name="tgl_agenda" id="tgl_agenda" placeholder="">
                         </div>
                     </div>
                 </div>
@@ -78,7 +82,7 @@ $(document).ready(function(){
                     <div class="form-group row">
                         <label for="colFormLabel" class="col-sm-3 col-form-label">Tanggal Surat</label>
                         <div class="col-sm-7">
-                        <input type="date" class="form-control" id="colFormLabel" placeholder="">
+                        <input type="date" class="form-control" name="tgl_surat" id="tgl_surat" placeholder="">
                         </div>
                     </div>
                 </div>
@@ -88,7 +92,7 @@ $(document).ready(function(){
                     <div class="form-group row">
                         <label for="colFormLabel" class="col-sm-2 col-form-label">Asal Surat</label>
                         <div class="col-sm-9">
-                        <input type="text" class="form-control" id="colFormLabel" placeholder="">
+                        <input type="text" class="form-control" name="asal_surat" id="asal_surat" placeholder="">
                         </div>
                     </div>
                 </div>
@@ -100,7 +104,7 @@ $(document).ready(function(){
                         <div class="col-sm-9">
                         <textarea
                             class="form-control"
-                            id="exampleTextarea1"
+                            id="exampleTextarea1" name="perihal"
                             rows="4"
                           ></textarea>
                         </div>
@@ -112,7 +116,7 @@ $(document).ready(function(){
                     <div class="form-group row">
                         <label for="colFormLabel" class="col-sm-2 col-form-label">Status</label>
                         <div class="col-sm-9">
-                        <input type="text" class="form-control" id="colFormLabel" placeholder="">
+                        <input type="text" class="form-control" name="status_surat" id="status_surat" placeholder="">
                         </div>
                     </div>
                 </div>
@@ -122,7 +126,7 @@ $(document).ready(function(){
                     <div class="form-group row">
                         <label for="colFormLabel" class="col-sm-4 col-form-label" for="inlineFormCustomSelect">Tingkat Keamanan</label>
                         <div class="col-sm-7">
-                        <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                        <select class="custom-select mr-sm-2" name="tk_keamanan" id="inlineFormCustomSelect">
                             <option selected>-</option>
                             <option value="1">Biasa</option>
                             <option value="2">Penting</option>
@@ -135,7 +139,7 @@ $(document).ready(function(){
                     <div class="form-group row">
                         <label for="colFormLabel" class="col-sm-3 col-form-label">Lampiran</label>
                         <div class="col-sm-7">
-                        <input type="text" class="form-control" id="colFormLabel" placeholder="">
+                        <input type="text" class="form-control" name="lampiran" id="colFormLabel" placeholder="">
                         </div>
                     </div>
                 </div>
@@ -146,8 +150,8 @@ $(document).ready(function(){
                         <label for="colFormLabel" class="col-sm-2 col-form-label">Dokumen</label>
                         <div class="col-sm-9">
                         <div class="custom-file">
-                            <input type="file" class="custom-file-input" id="inputGroupFile01">
                             <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                            <input type="file" class="custom-file-input" name="file_pdf" id="colFormLabel">
                         </div>
                         
                         </div>
@@ -157,12 +161,29 @@ $(document).ready(function(){
             <br>
             <div class="form-group row">
                 <div class="col-sm-12">
-                <button type="submit" class="btn btn-primary">Tambah Surat</button>
+                <button class="btn btn-primary" name="tambah">Tambah Surat</button>
                 </div>
             </div>
             </form>
+            <?php
+
+                        if(isset($_POST['tambah'])){
+                            $namabukti = $_FILES["file_pdf"]["name"];
+                            $lokasibukti = $_FILES["file_pdf"]["tmp_name"];
+                            $namafiks = date("YmdHis").$namabukti;
+                            move_uploaded_file($lokasibukti, "berkas/$namafiks");
+                            // $direktori = "berkas/";
+                            // $file_pdf=$_FILES['file_pdf']['name'];
+                            // move_uploaded_file($_FILES['file_pdf']['tmp_name'],$direktori.$file_pdf);
+                            $conn->query("INSERT INTO surat_masuk (no_agenda,tk_keamanan,tgl_agenda,no_surat,tgl_surat,asal_surat,perihal,lampiran,status_surat,file_pdf)
+                            VALUES('$_POST[no_agenda]','$_POST[tk_keamanan]','$_POST[tgl_agenda]','$_POST[no_surat]','$_POST[tgl_surat]','$_POST[asal_surat]','$_POST[perihal]','$_POST[lampiran]','$_POST[status_surat]','$_POST[file_pdf]')");
+
+                        echo "<script>alert( 'Berhasil Tambah Data' );</script>";
+                        echo "<script>location = 'operator.php';</script>";
+                        }
+
+                    ?>
         </div>
     </div>  
 </div>   
 </body>
-</html>
