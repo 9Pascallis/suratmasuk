bde0968a2cc200c04b8d55ae69e27bc90a75f32f
9Pascallis <pascallishenoch@gmail.com>
2022-05-23 14:02:49 +0700

    revisi

--- suratmasuk/kepala.php
+++ suratmasuk/kepala.php
@@ -13,7 +13,7 @@ session_start();
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="suratmasuk/public/css/operator.css">
+<link rel="stylesheet" type="text/css" href="public/css/operator.css">
 <link rel="shortcut icon" href="public/img/logo.png" />
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
