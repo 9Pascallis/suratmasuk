3a06a03e27072b998d117246029538e99b82a9b8
9Pascallis <pascallishenoch@gmail.com>
2022-05-24 21:27:34 +0700

    revisi

---
+++ suratmasuk/proses-login.php
@@ -0,0 +1,20 @@
+<?php 
+include "../database/db.php";
+session_start();
+
+if (isset($_POST['login'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $query = mysqli_query($conn,"select * from user where username='$username' and password='$password'");
+    if(mysqli_num_rows($query) !== 0){
+        $_SESSION["user"] = $query->fetch_assoc();
+        header("location:index.php");
+
+    }else{
+        echo '<script>alert("Login Gagal!!!");location.href="login.php"</script>';
+    }
+    }else{
+    header("location:login.php?pesan=gagal");
+}
+
+?>
