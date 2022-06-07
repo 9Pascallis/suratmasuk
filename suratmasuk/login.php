1de586212850a9f4520c3c20dd67f5e2aafecedb
9Pascallis <pascallishenoch@gmail.com>
2022-06-07 14:43:06 +0700

    revisi login

--- suratmasuk/login.php
+++ suratmasuk/login.php
@@ -9,62 +9,28 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="../public/css/login.css">
 </head>
-<body class="login-page">
-	<section class="h-100">
-		<div class="container h-100">
-			<div class="row justify-content-md-center h-100">
-				<div class="card-wrapper">
-					<div class="card fat">
-						<div class="card-body">
-							<div class="text-center">
-								<img src="../public/img/logo.png" alt="logo" height="50px" width="50px">
-							</div>
-							<h4 class="text-center">Login</h4>
-							<p class="text-center">Surat Masuk</p>
-							<br>
-							<form action="proses-login.php" method="POST" class="login-validation" novalidate="">
-								<div class="form-group">
-									<label for="text">Username</label>
-									<input  type="text" class="form-control" name="username" value="" required autofocus>
-									<div class="invalid-feedback">
-										Email is invalid
-									</div>
-								</div>
-								<div class="form-group">
-									<label for="password">Password
-									</label>
-									
-									<input  type="password" class="form-control"  name="password" required data-eye>
-								    <div class="invalid-feedback">
-								    	Password is required
-							    	</div>
-								</div>
 
-								<div class="form-group">
-									<div class="custom-checkbox custom-control">
-										<input type="checkbox" name="remember" id="remember" class="custom-control-input">
-										<label for="remember" class="custom-control-label">Remember Me</label>
-									</div>
-								</div>
+<div class="login-box">
+    <img src="../public/img/logo.png" alt="logo" height="70px" width="70px" class="center">
+    <br>
+  <h2>LOGIN</h2>
+  <h3>SURAT MASUK</h3>
+  <form action="proses-login.php" method="POST" class="login-validation" novalidate="">
+  <br><br><br>
+  <form>
+    <div class="user-box">
+      <input type="text" name="username" required="">
+      <label>Username</label>
+    </div>
+    <div class="user-box">
+      <input type="password" name="password" required="">
+      <label>Password</label>
+    </div>
+    <div class="text-center">
+        <button type="submit" name="login" class="btn btn-primary btn-block" >Login</button>
+    </div>
+  </form>
+</div>
 
-								<div class="form-group m-0">
-									<button type="submit" name="login" class="btn btn-primary btn-block">
-										Login
-									</button>
-								</div>
-								<div class="mt-4 text-center">
-								</div>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	<script src="public/js/login.js"></script>
 </body>
 </html>
