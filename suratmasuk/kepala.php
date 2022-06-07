b7d8825db7c944ab245ab5325a3031c0ab6d238b
9Pascallis <pascallishenoch@gmail.com>
2022-06-07 11:10:47 +0700

    revisi

--- suratmasuk/kepala.php
+++ suratmasuk/kepala.php
@@ -5,68 +5,120 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
+
+<!--HEAD-->
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>Home | Surat Masuk</title>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="../public/css/operator.css">
-<link rel="shortcut icon" href="../public/img/logo.png" />
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Kepala | Surat Masuk</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="../public/img/logo.png" rel="icon">
+  <link href="../public/img/logo.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- vendors CSS Files -->
+  <link href="../public/vendors/aos/aos.css" rel="stylesheet">
+  <link href="../public/vendors/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../public/vendors/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../public/vendors/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../public/vendors/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="../public/vendors/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../public/vendors/swiper/swiper-bundle.min.css" rel="stylesheet">
+  
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="shortcut icon" href="public/img/logo.png" />
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+  <!-- Main CSS File -->
+  <link href="../public/css/indexsuratmasuk.css" rel="stylesheet">
+</head>
+<!--END HEAD-->
 
 <script>
 $(document).ready(function(){
 	$('[data-toggle="tooltip"]').tooltip();
 }); 
 </script>
-</head>
-<body>
-<!-- Bootstrap Static Header -->
-<div style="background: url(../public/img/bg.jpg)" img src="../public/img/bg.jpg" class="bg-cover text-white">
-    <div class="header text-center">
-        <br><h4 class="header1 font-weight-bold">Daftar Surat Masuk BMKG Juanda</h4>
-    </div>
-</div>
 
-<div class="containerbox">
+<!--BODY-->
+<body>
+  <!-- ======= Header1 ======= -->
+  <header id="header1" class="fixed-top header1-inner-pages">
+    <div class="container d-flex align-items-center justify-content-lg-between">
+      <h1 class="logo me-auto me-lg-0"><a href="../index.php"><img src="../public/img/logo.png" style="width:40px;height:40px;"> BMKG <span>JUANDA</span></a></h1>
+        <nav id="navbar1" class="navbar1 order-last order-lg-0">
+          <ul>
+            <li><a class="nav-link" href="../index.php">Home</a></li>
+            <li class="dropdown"><a href="#"><span>Surat</span> <i class="bi bi-chevron-down"></i></a>
+              <ul>
+                <li><a href="../suratmasuk/login.php">Surat Masuk</a></li>
+                <li><a href="../suratmasuk/login.php">Surat Keluar</a></li>
+              </ul>
+            <li class="dropdown"><a href="#"><span>SPT</span> <i class="bi bi-chevron-down"></i></a>
+              <ul>
+                <li><a href="sptjam.php">SPT Jam</a></li>
+                <li><a href="sptharian.php">SPT Harian</a></li>
+              </ul>
+            </li>
+            <li><a class="nav-link" href="https://juanda.jatim.bmkg.go.id/tata-usaha/usulan/login.php">Usulan</a></li>
+            <li><a class="nav-link scrollto" href="https://juanda.jatim.bmkg.go.id/tata-usaha/cuti/index.php">Pengajuan Cuti</a></li>
+            <li><a class="nav-link scrollto" href="https://docs.google.com/forms/d/1FFw69b9mUdI0VTd5VBa_yvCi5DoXNtH66P3r12tjrXE/viewform?edit_requested=true">IP ASN</a></li>
+          </ul>
+          <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+    </div>
+  </header>
+  <!-- ======= End Header ======= -->
+
+
+
+  <!-- ======= Main ======= -->
+  <main id="main">
+    <!-- ======= breadcrumbs1 ======= -->
+    <section class="breadcrumbs1">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2><b>Surat Masuk | Daftar Surat</b></h2>
+          <ol>
+            <li><a href="../index.php">Home</a></li>
+            <li>Surat Masuk</a></li>
+          </ol>
+        </div>
+      </div>
+    </section>
+    <!-- ======= End breadcrumbs1 ======= -->
+
+    <!-- ======= Inner Page ======= -->
+    <div class="containerbox">
     <div class="table-responsive">
-        <div class="table-wrapper">
-            <div class="table-title">	
-            <h4><p class="text-center">Bulan Maret 2022</p></h4>
-            <br>
-                <div class="row">
-                        <div class="col-sm-6">
-                         </div> 
-                        <div class="col-sm-6">
-                            <form action="" method="post">
-                            <div class="input-group">
-                                <input type="text" name="keyword" class="form-control" placeholder="Masukan kata kunci pencarian" autocomplete="off" autofocus>
-                                <div class="input-group-append"></div>
-                                    <button type="submit" name="cari" class="btn btn-secondary pl-4 pr-4">Cari</button>
-                                </div>
-                                </form>
-                        </div>
-                </div>
-            </div>
+    <h3><p class="text-center"><b>Bulan Maret 2022</b></p></h3>
+    <br><br>
             <table class="table table-striped table-hover table-bordered">
                 <thead>
                     <tr>
-                        <th width="50px">No</th>
+                        <th width="10px">No</th>
                         <th width="10px">No Agenda</th>
-                        <th width="50px">Tanggal Agenda</th>
+                        <th width="10px">Tanggal Agenda</th>
                         <th width="10px">Tingkat Keamanan</th>
-                        <th width="50px">Tanggal Surat</th>
-                        <th width="20px">No Surat</th>
-                        <th width="50px">Asal Surat</th>
-                        <th width="300px">Perihal</th>
-                        <th width="50px">Lampiran</th>
-                        <th width="150px">Disposisi</th>
-                        <th width="5px">Hasil</th>
+                        <th width="10px">Tanggal Surat</th>
+                        <th width="10px">No Surat</th>
+                        <th>Asal Surat</th>
+                        <th>Perihal</th>
+                        <th>Lampiran</th>
+                        <th>Hasil</th>
+                        <th width="120px">Disposisi</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -119,12 +171,12 @@ $(document).ready(function(){
                         <td><?=$per?></td>
                         <td><?=$lmpr?></td>
                         <td>
-                            <a href="#" class="view" title="Disposisi" data-toggle="tooltip"><i class="material-icons">&#xE431;</i></a>
-                            <a href="#" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                            <a href="../suratmasuk/delete_masuk.php?id=<?php echo $data['id'];?>" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
+                            <a href="../suratmasuk/disposisirev.php" class="view" title="View" data-toggle="tooltip"><i class="material-icons">&#xE417;</i></a>
                         </td>
                         <td>
-                            <a href="#" class="view" title="View" data-toggle="tooltip"><i class="material-icons">&#xE417;</i></a>
+                        <a href="../suratmasuk/disposisirev.php" class="view" title="Read" data-toggle="tooltip"><i class="material-icons">&#xE431;</i></a>
+                            <a href="../suratmasuk/editsurat.php?id=<?php echo $data['id'];?>" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
+                            <a href="../suratmasuk/delete_masuk.php?id=<?php echo $data['id'];?>" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
                         </td>
                     </tr>
                 <?php
@@ -171,5 +223,37 @@ $(document).ready(function(){
         </div>
     </div>  
 </div>   
+<!-- ======= End Inner Page ======= -->
+  </main>
+  <!-- ======= End Main ======= -->
+
+
+
+  <!-- ======= Footer1 ======= -->
+  <footer id="footer1">
+    <div class="container">
+      <div class="copyright1">
+        &copy; Copyright <strong><span>2022 BMKG Juanda</span></strong>. All Rights Reserved.
+      </div>
+    </div>
+  </footer>
+  <!-- ======= End Footer1 ======= -->
+
+
+
+
+  <!-- vendors JS Files -->
+  <script src="public/vendors/purecounter/purecounter.js"></script>
+  <script src="public/vendors/aos/aos.js"></script>
+  <script src="public/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="public/vendors/glightbox/js/glightbox.min.js"></script>
+  <script src="public/vendors/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="public/vendors/swiper/swiper-bundle.min.js"></script>
+  <script src="public/vendors/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="public/js/main.js"></script>
+
 </body>
+<!--END BODY-->
 </html>
--- suratmasuk/tambahsurat.php
+++ suratmasuk/tambahsurat.php
@@ -43,7 +43,7 @@ if (isset($_POST["tambah"])) {
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="../public/css/tambah.css">
+<link rel="stylesheet" type="text/css" href="../public/css/tambahsurat.css">
 <link rel="shortcut icon" href="../public/img/logo.png" />
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
